4b12ddaa6d56ae1a8addc3668f327a499e27c78d
Jimmy <gosu.snp@gmail.com>
2013-08-06 23:08:06 +0200

    Reduce memory usage.

--- include/node.hh
+++ include/node.hh
@@ -25,8 +25,10 @@
 /*
 ** Node implementation for a TernarySearchTree
 **
-** Lot of optimisations can be done.
-** Memory usage is far from optimal.
+** Implementation notes
+**
+** data is stored on nodes where key_ == 0, this allows to use the
+** lokid_ field to store the value since it will never be used.
 */
 template <typename charT, typename valueT>
 class Node
@@ -40,11 +42,11 @@ public:
         lokid_(0),
         eqkid_(0),
         hikid_(0),
-        value_(0),
         key_(key)
     { }
     ~Node() {
-        delete lokid_;
+        if (key_)          // Only delete lokid_ when key_ is not null
+            delete lokid_; // otherwise, it means that lokid_ is a value_type*
         delete eqkid_;
         delete hikid_;
     }
@@ -52,24 +54,34 @@ public:
     template <typename T>
     value_type* insert(T cp, value_type* value, bool replace) {
         if (*cp < key_) {
-            if (!this->lokid_)
-                this->lokid_ = new Node(*cp);//, cp, value, replace);
+            if (!lokid_)
+                lokid_ = new Node(*cp);
             return lokid_->insert(cp, value, replace);
         } else if (*cp > key_) {
             if (!hikid_)
-                hikid_ = new Node(*cp);//, cp, value, replace);
+                hikid_ = new Node(*cp);
             return hikid_->insert(cp, value, replace);
         } else {
             if (*cp) {
                 if (!eqkid_)
-                    eqkid_ = new Node(*cp);//, ++cp, value, replace);
+                    eqkid_ = new Node(*cp);
                 return eqkid_->insert(++cp, value, replace);
             } else {
-                if (value_ && !replace)
-                    return value_;
-                return value_ = value;
+                if (get_value() && !replace)
+                    return get_value();
+                return set_value(value);
             }
         }
+    }
+
+    value_type* get_value() const
+    { // See implementation notes
+        return reinterpret_cast<value_type*>(lokid_);
+    }
+    value_type* set_value(value_type* value)
+    { // See implementation notes
+        lokid_ = reinterpret_cast<node_type*>(value);
+        return reinterpret_cast<value_type*>(lokid_);
     }
 
     /*
@@ -104,7 +116,7 @@ public:
         value_type* value() const
         {
             const node_type* np = search_node_(0);
-            return np ? np->value_ : 0;
+            return np ? np->get_value() : 0;
         }
 
     protected:
@@ -148,7 +160,7 @@ public:
             if (*cp) {
                 return eqkid_ ? eqkid_->search(++cp) : 0;
             } else {
-                return value_;
+                return get_value();
             }
         }
     }
@@ -157,7 +169,6 @@ protected:
     node_type*  lokid_;
     node_type*  eqkid_;
     node_type*  hikid_;
-    value_type* value_;
     char_type   key_;
 
 }; // End of class Node
